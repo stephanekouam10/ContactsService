78b4b5d24c059c87ff595608d1ae56b337333b93
stephane kouam <stephane.kouam237@gmail.com>
2023-07-06 11:42:14 +0200

    add integrationTests

--- tests/integrationTests/ContactServiceIntegrationTest.php
+++ tests/integrationTests/ContactServiceIntegrationTest.php
@@ -31,23 +31,72 @@ final class ContactServiceIntegrationTest extends TestCase
     // test de suppression de toute les données, nécessaire pour nettoyer la bdd de tests à la fin
     public function testDeleteAll()
     {
+        $this->contactService->createContact('abc', 'def');
+        $this->contactService->createContact('abc', 'def');
+        $this->contactService->createContact('abc', 'def');
+
+        $this->contactService->deleteAllContact();
+        $response = $this->contactService->getAllContacts();
+        $this->assertIsArray($response);
+        $this->assertEquals(0, count($response));
     }
 
+    public function testGetAllContact()
+    {
+        $this->contactService->createContact('abc', 'def');
+        $this->contactService->createContact('abc', 'def');
+        $this->contactService->createContact('abc', 'def');
+
+        $response = $this->contactService->getAllContacts();
+        $this->assertIsArray($response);
+        $this->assertEquals(3, count($response));
+        $this->testDeleteAll();
+    }
 
     public function testCreationContact()
     {
+        $this->contactService->createContact('abcx', 'def');
+
+        $response = $this->contactService->searchContact('abcx');
+        $this->assertEquals('abcx', $response[0]['nom']);
+        $this->testDeleteAll();
     }
 
     public function testSearchContact()
     {
+        $this->contactService->createContact('abcx', 'def');
+        $response = $this->contactService->searchContact('abcx');
+        $this->assertIsArray($response);
+        $this->assertEquals(1, count($response));
+        $this->testDeleteAll();
+    }
+
+    public function testGetContact()
+    {
+        $this->contactService->createContact('abcx', 'def');
+        $response = $this->contactService->getContact(1);
+        $this->assertSame('abcx', $response["nom"]);
+        $this->assertSame('def', $response["prenom"]);
+        $this->testDeleteAll();
     }
 
     public function testModifyContact()
     {
+        $this->contactService->createContact('abcx', 'def');
+        $this->contactService->updateContact(1,'toto', 'tata');
+        $response = $this->contactService->getContact(1);
+        $this->assertSame('toto', $response["nom"]);
+        $this->assertSame('tata', $response["prenom"]);
+        $this->testDeleteAll();
     }
 
     public function testDeleteContact()
     {
+        $this->contactService->createContact('abcx', 'def');
+        $this->contactService->deleteContact(1);
+        $response = $this->contactService->getAllContacts(1);
+        $this->assertSame(0, count($response));
+        $this->testDeleteAll();
     }
 
 }
